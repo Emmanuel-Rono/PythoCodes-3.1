37ee4e4e7d5f08cd9695ffa4af913d78c5b83bc2
ronoemmanuel933@gmail.com <you@example.com>
2022-01-24 10:07:41 +0300

    Monday 24 Notes

---
+++ numericExpression.py
@@ -0,0 +1,45 @@
+#operators
+from math import remainder
+
+
+c="Ëmmanuel Rono"
+a=100
+b=51
+#add
+sum=a+b
+#subbtrat=ct
+subtract=a-b
+#times
+times=a*b
+#division
+division=a/b
+#power
+power=a**b
+
+#remainder
+remainder=a%b
+#to print
+print("sum=",sum)
+print("subtract=",subtract)
+print("times=",times)
+print("division=",division)
+print("power=",power)
+print("remainder",remainder)
+#To check the datatype
+"""To print the datatype"""
+print(type(a),(b))
+print(type(c))
+"""Type conversion"""
+#to convert to int
+age =int(input("Enter your Age: "))
+print("You are", age)
+
+#String
+name=(input("Your name"))
+print("You are: ",name)
+
+
+
+
+
+
